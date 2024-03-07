66adcdf87abbc81a06659708782c1034045f9c19
Matthew Polk <matthewpolkofficial@gmail.com>
2024-03-07 10:34:31 -0600

    First commit.

---
+++ client.py
@@ -0,0 +1,38 @@
+'''
+This is where the player/client will connect and send over to the server a guess of a 'char'.
+'''
+import sys
+
+# Import socket library
+from socket import *
+
+# Set hostname or IP address from command line or default to localhost
+# Set port number by converting argument string to integer or use default
+# Use defaults
+if sys.argv.__len__() != 3:
+    serverName = 'localhost'
+    serverPort = 25730 #5555
+# Get from command line
+else:
+    serverName = sys.argv[1]
+    serverPort = int(sys.argv[2])
+
+# Choose SOCK_STREAM, which is TCP
+clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# Connect to server using hostname/IP and port
+clientSocket.connect((serverName, serverPort))
+
+# Get sentence from user
+sentence = input('Input lowercase sentence: ')
+
+# Send it into socket to server
+sentenceBytes = sentence.encode('utf-8')
+clientSocket.send(sentenceBytes)
+
+# Receive response from server via socket
+modifiedSentence = clientSocket.recv(1024)
+
+print('From Server: {0}'.format(modifiedSentence.decode('utf-8')))
+
+clientSocket.close()
--- server.py
+++ server.py
@@ -1,5 +1,66 @@
+'''This Server shall hold all of the game intelligence, while the player/client should do
+the work of sending in characters in this game to guess ARKANSAS. Below is Hangman logic;
+
+
+ |----|     <-- 1.
+ |    0     <-- 2.
+ |   -|-    <-- 6, 3, 7.
+ |   / \    <-- 4, 5.
+_|____
+
+'''
+
 import sys
+
+# Import socket library
 from socket import *
 
+goal = 'Arkansas'
+guess = ''
+
+# Set port number by converting argument string to integer
+# If no arguments set a default port number
+# Defaults
 if sys.argv.__len__() != 2:
-    serverPort = 25730
+    serverPort = 25730 #5555
+# Get port number from command line
+else:
+    serverPort = int(sys.argv[1])
+
+# Choose SOCK_STREAM, which is TCP
+# This is a welcome socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# The SO_REUSEADDR flag tells the kernel to reuse a local socket
+# in TIME_WAIT state, without waiting for its natural timeout to expire.
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+# Start listening on specified port
+serverSocket.bind(('', serverPort))
+
+# Listener begins listening
+serverSocket.listen(1)
+
+print("The server is ready to receive")
+
+# Forever, read in sentence, convert to uppercase, and send
+while 1:
+    # Wait for connection and create a new socket
+    # It blocks here waiting for connection
+    connectionSocket, addr = serverSocket.accept()
+
+    # Read bytes from socket
+    sentence = connectionSocket.recv(1024)
+  
+    # Convert sentence to uppercase
+    # Note you can often send strings directly and it will work but
+    # you should be aware that it does not always act correct.
+    sentenceString = sentence.decode('utf-8')
+    capitalizedSentence = sentenceString.upper()
+    capitalizedSentenceBytes = capitalizedSentence.encode('utf-8')
+  
+    # Send it into established connection
+    connectionSocket.send(capitalizedSentenceBytes)
+  
+    # Close connection to client but do not close welcome socket
+    connectionSocket.close()
